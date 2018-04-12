12bb0a7bc179a472f27324d52d0b7d61ab8f7a7f
Andreas Mann <andreasmann94@gmail.com>
2018-04-12 15:01:18 +0200

    Styled the UI

--- App/Page/DetailPage.js
+++ App/Page/DetailPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button} from "react-native";
 import Equation from '../Model/Equation.js';
 
 export default class DetailPage extends React.Component {
@@ -12,17 +12,102 @@ export default class DetailPage extends React.Component {
     super(props);
 
     this.equation = props.navigation.state.params.equation
-    console.log(this.equation)
+
+    this.state={
+      parameterArray: Array(this.equation.parameters.length).fill(""),
+      calculateResult: "Fill in values & calculate!"
+    }
   }
 
   render() {
+
+    var parameters = [];
+    for(let i = 0; i < this.equation.parameters.length; i++){
+      parameters.push(
+        <View style = {{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          paddingTop: 10,
+          paddingBottom: 10
+        }} key={i}>
+          <Text style={{
+            flex: 1,
+            fontSize: 20,
+            marginLeft: '5%'
+          }}>{this.equation.parameters[i]}</Text>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              flex: 3,
+              marginRight: '20%'
+            }}
+            onChangeText={(text) => this.onParametersInput(i, text)}
+            value={this.state.parameterArray[i]}
+          />
+        </View>)
+    }
+
     return (
       <View>
-        <Text>{this.equation.name}</Text>
-        <Text>{this.equation.description}</Text>
-        <Text>{this.equation.equation}</Text>
+        <Text style = {{
+          marginLeft: '5%',
+          fontSize: 20,
+          paddingTop: 30,
+          paddingBottom: 10
+        }}>{this.equation.description}</Text>
+        <Text style = {{
+          fontSize: 16,
+          fontWeight: 'bold',
+          marginLeft: '5%'
+        }}>{this.equation.equation}</Text>
+
+        <View style={{
+          paddingVertical: 16,
+          paddingHorizontal: 8,
+        }}>
+          {parameters}
+        </View>
+
+        <Text>{this.state.calculateResult}</Text>
+
+        <Button
+          onPress={ () => this.onCalculatePress()}
+          title="Calculate"
+        />
       </View>
     )
+  }
+
+  onParametersInput(index, text) {
+    const parameterArray = this.state.parameterArray
+    parameterArray[index] = text
+    this.setState({
+      parameterArray
+    })
+  }
+
+
+  onCalculatePress() {
+    var calculateResult = ""
+
+    for(let i = 0; i < this.state.parameterArray.length; i++){
+
+      if(i == this.state.parameterArray.length-1) {
+        calculateResult += this.state.parameterArray[i]
+      } else {
+        calculateResult += this.state.parameterArray[i] + this.equation.expressions[i]
+      }
+
+    }
+
+    calculateResult = eval(calculateResult)
+    this.setState({
+      calculateResult
+    })
+
   }
 
 }
--- App/Page/HomePage.js
+++ App/Page/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, FlatList, ActivityIndicator, Dimensions } from "react-native";
+import { View, Text, FlatList, ActivityIndicator, Dimensions, TouchableHighlight } from "react-native";
 import { List, ListItem, SearchBar } from "react-native-elements";
 import { StackNavigator } from 'react-navigation';
 import Equation from '../Model/Equation.js';
@@ -15,9 +15,9 @@ export default class HomePage extends React.Component {
   constructor(props) {
     super(props);
 
-    var eq1 = new Equation(1, "Momentum", "Calclate momentum", "p = m * v")
-    var eq2 = new Equation(2, "Force", "May the force be with you", "F = dp * dt")
-    var eq3 = new Equation(3, "Velocity", "WoW", "v = dr / dt")
+    const eq1 = new Equation(1, "Momentum", "Calclate momentum", "p = m * v", ["m", "v"], ["*"])
+    const eq2 = new Equation(2, "Force", "May the force be with you", "F = dp * dt / dv", ["dp", "dt", "dv"], ["*", "/"])
+    const eq3 = new Equation(3, "Velocity", "WoW", "v = dr / dt", ["dr", "dt"], ["/"])
 
     this.equations = [eq1, eq2, eq3]
   }
@@ -29,26 +29,37 @@ export default class HomePage extends React.Component {
           height: 1,
           width: "100%",
           backgroundColor: "#CED0CE",
-          marginLeft: "5%"
         }}
       />
     );
   };
 
   render() {
+
     return (
       <View>
         <List containerStyle={{ height, marginTop: 0, borderTopWidth: 0, borderBottomWidth: 0 }}>
           <FlatList
             data={this.equations}
             renderItem={({ item }) => (
-              <ListItem
-                title={`${item.name}`}
-                subtitle={item.equation}
-                containerStyle={{ borderBottomWidth: 0 }}
-                onPress={()=> { this.onPress(item) }}
-              />
-            )}
+              // <TouchableHighlight
+              //   style = {{color: 'red'}}>
+                <ListItem
+                  title = {item.name}
+                  titleStyle = {{
+                    fontSize: 30,
+                    paddingTop: 10,
+                    paddingBottom: 10
+                  }}
+                  subtitle = {item.equation}
+                  subtitleStyle = {{
+                    fontSize: 24
+                  }}
+                  containerStyle={{ borderBottomWidth: 0 }}
+                  onPress={()=> { this.onPress(item) }}
+                />
+                // </TouchableHighlight>
+              )}
             keyExtractor={item => item.id}
             ItemSeparatorComponent={this.renderSeparator}
           />
