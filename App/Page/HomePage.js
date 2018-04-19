3a6539c319995d1e1fc53df7c60618c7bf99338f
Martin Tran <martin_t_39@hotmail.com>
2018-04-19 19:05:29 +0200

    Add sort list by draging the items

--- App/Page/HomePage.js
+++ App/Page/HomePage.js
@@ -1,22 +1,60 @@
 import React, { Component } from "react";
-import { View, Text, FlatList, ActivityIndicator, Dimensions, TouchableHighlight, StyleSheet } from "react-native";
+import { View, Text, ActivityIndicator, Dimensions, TouchableHighlight, StyleSheet, AsyncStorage, Button} from "react-native";
 import { List, ListItem, SearchBar } from "react-native-elements";
 import { StackNavigator } from 'react-navigation';
+import SortableListView from 'react-native-sortable-listview'
 
 var height = Dimensions.get('window').height;
 
 export default class HomePage extends React.Component {
 
-  static navigationOptions = {
-    title: 'Equations',
+  static navigationOptions = ({ navigation }) => {
+      const { params = {} } = navigation.state;
+      const buttonTitle = "Order"
+
+      return {
+        title: "Equations",
+        headerRight: <Button title={buttonTitle} onPress={() => params.editOrder()} />
+      };
   };
+
+  _editOrder = () => {
+      this.setState({ isOrder: !this.state.isOrder})
+  }
+
+  componentWillMount() {
+    this.props.navigation.setParams({ editOrder: this._editOrder });
+  }
 
   constructor(props) {
     super(props);
 
     var customData = require('../Model/Equations.json')
-    this.equations = customData["Equations"]
+    const equations = customData["Equations"]
 
+    this.state={
+      equations: equations,
+      order: null,
+      isOrder: false
+    }
+
+    // The order is stored in index.
+    this.setOrder()
+
+  }
+
+  // Fetch the order from the locale storage. If there is none there, use standard.
+  async setOrder() {
+    try {
+      const value = await AsyncStorage.getItem('@MySuperStore:equationOrder');
+      if (value !== null){
+        const order = JSON.parse(value)
+        this.setState({order})
+      }
+    } catch (error) {
+      const order = Object.keys(this.state.equations) // Array of keys, defaults
+      this.setState({order})
+    }
   }
 
   renderSeparator = () => {
@@ -28,35 +66,47 @@ export default class HomePage extends React.Component {
   render() {
 
     return (
-      <View>
-        <List containerStyle={styles.ListContainer}>
-          <FlatList
-            data={this.equations}
-            renderItem={({ item }) => (
-              // <TouchableHighlight
-              //   style = {{color: 'red'}}>
-                <ListItem
-                  title = {item.name}
-                  titleStyle = {styles.listTitle}
-                  subtitle = {item.equation}
-                  subtitleStyle = {styles.listSubTitle}
-                  containerStyle={styles.lsitItemContainer}
-                  onPress={()=> { this.onPress(item) }}
-                />
-                // </TouchableHighlight>
-              )}
-            keyExtractor={item => item.id}
-            ItemSeparatorComponent={this.renderSeparator}
-          />
-        </List>
-      </View>
+      <SortableListView
+        style={{ flex: 1 }}
+        data={this.state.equations}
+        order={this.state.order}
+        disableSorting= {!this.state.isOrder}
+        onRowMoved={e => {
+          const order = this.state.order
+          order.splice(e.to, 0, order.splice(e.from, 1)[0])
+          this.setState({order})
+          this.onRowMoved()
+        }}
+        renderRow={ item  => (
+            <ListItem
+              title = {item.name}
+              titleStyle = {styles.listTitle}
+              subtitle = {item.equation}
+              subtitleStyle = {styles.listSubTitle}
+              containerStyle={styles.listItemContainer}
+              rightIcon={{ name: 'chevron-right' }}
+              onPress={()=> { this.onPress(item) }}
+            />
+          )}
+      />
     );
   }
 
   onPress(equation) {
     this.props.navigation.navigate('Detail', {title: equation.name, equation});
   }
+
+  async onRowMoved() {
+
+      // Store the order in local storage when row is moved.
+      try {
+        await AsyncStorage.setItem('@MySuperStore:equationOrder', JSON.stringify(this.state.order));
+      } catch (error) {
+        console.log("Fail to store equation order!")
+      }
+  }
 }
+
 
 const styles = StyleSheet.create({
   seperator:
@@ -73,7 +123,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderBottomWidth: 0
   },
-  lsitItemContainer:
+  listItemContainer:
   {
     borderBottomWidth: 0
   },
