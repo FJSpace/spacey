f718057826fce79742c1d86bbc4a1ba87639b8da
valoczi.gyorgy <valoczi.gyorgy@gmail.com>
2018-04-20 17:05:59 +0200

    Snapshot test for HomePage

--- App/__tests__/HomePage-test.js
+++ App/__tests__/HomePage-test.js
@@ -2,14 +2,16 @@ import 'react-native';
 import React from 'react';
 import HomePage from '../Page/HomePage';
 import renderer from 'react-test-renderer';
+import { StackNavigator } from 'react-navigation';
 
 
-it('works', () => {
+test('works', () => {
   expect(1).toBe(1);
 });
 
-it('renderers correctly', () => {
+test('renderers correctly', () => {
 	const hello = renderer.create(
 		<HomePage/>
 	);
+	expect(hello).toMatchSnapshot();
 });
