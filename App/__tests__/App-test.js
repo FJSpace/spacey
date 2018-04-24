6de49460f30b813f24340696851fbff362185e1a
valoczi.gyorgy <valoczi.gyorgy@gmail.com>
2018-04-24 15:36:55 +0200

    Adding wrapper and shallow to App test

--- App/__tests__/App-test.js
+++ App/__tests__/App-test.js
@@ -5,6 +5,9 @@ import renderer from 'react-test-renderer';
 
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+  const wrapper = shallow(
+      <App />
+    );
+
+    expect(wrapper).toMatchSnapshot();
 });
