0f365cad8b910dd0bcf3d614db910d4c4e6eb342
Fredrik Johansson <frejohi@student.chalmers.se>
2018-05-11 10:12:42 +0200

    Only some more tests

--- App/__tests__/AddEquationPage-test.js
+++ App/__tests__/AddEquationPage-test.js
@@ -10,6 +10,10 @@ describe('Parsing tests', () => {
   const component = new AddEquationPage();
   const errorObject = data["Error"];
   const noEqual = data["NoEqual"];
+  const parentheses = data["Parentheses"];
+  const number = data["Number"];
+  const numAndParentheses = data["NumAndParentheses"];
+  
   test('test if input string gets returned as object with that string as equation attribute', () => {
     const parsed = component.parseJSON("K=mv+mp*c-e/f");
 
@@ -22,7 +26,7 @@ describe('Parsing tests', () => {
     expect(parsed["parameters"]).toEqual(parsedObject["parameters"]);
   })
 
-  test('test if input string can handle spaces', () => {
+  test('test if the parser can handle spaces', () => {
     const parsed = component.parseJSON("K= mv +mp*c-e/f");
 
     expect(parsed["parameters"]).toEqual(parsedObject["parameters"]);
@@ -46,17 +50,38 @@ describe('Parsing tests', () => {
     expect(parsed).toEqual(errorObject[2]);
   })
   
-  test('test if input string can handle no equal sign', () => {
+  test('test if the parser can handle no equal sign', () => {
     const parsed = component.parseJSON("m*v");
 
     expect(parsed["parameters"]).toEqual(noEqual["parameters"]);
     expect(parsed["equation"]).toEqual(noEqual["equation"]);
   })
   
-  test('test if input string can handle to have the equation on left hand side', () => {
+  test('test if the parser can handle to have the equation on left hand side', () => {
     const parsed = component.parseJSON("mv+mp*c-e/f=K");
 
     expect(parsed["parameters"]).toEqual(parsedObject["parameters"]);
     expect(parsed["equation"]).toEqual(parsedObject["equation"]);
+  })
+  
+  test('test if the parser can handle parentheses', () => {
+    const parsed = component.parseJSON("p=m*(r-k)");
+
+    expect(parsed["parameters"]).toEqual(parentheses["parameters"]);
+    expect(parsed["equation"]).toEqual(parentheses["equation"]);
+  })
+  
+  test('test if the parser can handle numbers', () => {
+    const parsed = component.parseJSON("p=m*5");
+
+    expect(parsed["parameters"]).toEqual(number["parameters"]);
+    expect(parsed["equation"]).toEqual(number["equation"]);
+  })
+  
+  test('test if the parser can handle numbers and parentheses', () => {
+    const parsed = component.parseJSON("p=m*(r-1)");
+
+    expect(parsed["parameters"]).toEqual(numAndParentheses["parameters"]);
+    expect(parsed["equation"]).toEqual(numAndParentheses["equation"]);
   })
 });
